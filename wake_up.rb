f2981e7f471311f393240ec2980b7cb4a142c9a1
AshleeKaye <ashleemk@hotmail.com>
2018-06-07 17:32:56 +0000

    automatically backed up by learn

---
+++ wake_up.rb
@@ -0,0 +1,9 @@
+def wake_up
+  puts "First my cat opens my door and wakes me up around the same time in the morning"
+  5.times do
+  puts "I stay half awake for about 5 minutes"
+  puts "I watch youtube for a few minutes"
+  puts "I go down stairs and eat breakfast"
+  puts "Then I go and get ready for the day"
+end
+wake_up
